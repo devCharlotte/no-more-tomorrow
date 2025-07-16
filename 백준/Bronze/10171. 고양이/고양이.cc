21b306d7c5235e2473ed4673d349f563286495a4
jo0on <workjh77@gmail.com>
2025-07-17 01:49:46 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+// boj 10171
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|";
+
+    return 0;
+}
